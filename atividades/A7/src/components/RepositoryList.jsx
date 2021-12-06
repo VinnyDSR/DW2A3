7e4664bdb2365689f0ea16b126b6a17f95d820de
VinnyDSR <r.viniciusds@gmail.com>
2021-12-05 22:00:08 -0300

    A7

---
+++ atividades/A7/src/components/RepositoryList.jsx
@@ -0,0 +1,21 @@
+import {RepositoryItem} from "./RepositoryItem"
+import '../styles/repositories.scss'
+
+const repository = {
+    name: "Atividade",
+    description: "Descrição da Atividade",
+    link: "https://github.com/santicioli"
+}
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                < RepositoryItem repository={repository}/>
+                < RepositoryItem repository={repository}/>
+                < RepositoryItem repository={repository}/>
+                < RepositoryItem repository={repository}/>
+            </ul>
+        </section>
+    )
+}
